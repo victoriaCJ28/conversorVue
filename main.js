e9c60f78f3c8ef6da4b16da137cc834a58d003e4
VictoriaCJ28 <victoria.akicastro92@gmail.com>
2019-09-02 05:23:37 -0600

    Se anexa guia y se corrigen errores

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = new Vue({
     // arreglo con las variables de entrada y salida de datos que vienen desde la vista
     data: () => ({
         // iniciamos base 10 para que tome este valor por defecto que equivale a decimal
-        baseSeleccionada: 10,
+        baseSeleccionada: "10",
         // datos de salida
         binario: "",
         octal: "",
@@ -17,11 +17,15 @@ const app = new Vue({
     }),
 
     methods: {
+        // Con esta funcion vamos a convertir un decimal a todas sus otras bases
         numeroCambiado() {
+            // La funcion parseInt recibe el numero y la base como parametro.
             this.convertirDeDecimalA(parseInt(this.numero, this.baseSeleccionada));
         },
         
+        // en esta funcion asignamos las bases numericas a convertir 
         convertirDeDecimalA(numero) {
+            // como validacion convetimos los valores de las bases a cadenas
             if (!numero)
                 return;
             this.binario = numero.toString("2");
@@ -29,15 +33,15 @@ const app = new Vue({
             this.decimal = numero.toString("10");
             this.hexadecimal = numero.toString("16");
         },
-       
-       
-     
+ 
     },
-   
+   // este atributo de vue, permite ejecutar acciones sindeclarar eventos por lo que nos ayuda a mostrar los valores rapidamente
     watch: {
+        // Si elegimos otra base esta funcion ejecuta la conversion a todas las otras bases
         baseSeleccionada() {
             this.numeroCambiado();
         },
+        // esta funcion ejecuta automaticamente la conversion de cada numero ingresado
         numero() {
             this.numeroCambiado();
         }
